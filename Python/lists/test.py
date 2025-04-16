14b0d0a15192f7893d58e455a9a716954fed4b8b
Oleksii <celect1989@gmail.com>
2025-04-16 11:29:34 +0300

    LeetCode - next tasks v=1.0.0.2

--- Python/lists/test.py
+++ Python/lists/test.py
@@ -1,13 +1,6 @@
-grid = [[3, 2, 1], [1, 7, 6], [2, 7, 7]]
-n = len(grid)
-hash = {}
-pairs = 0
+head = [1,2,3,4]
 
-for i in range(n):
-    row = tuple(grid[i])
-    hash[row] = hash.get(row, 0) + 1
-
-for j in range(n):
-    column = tuple(grid[i][j] for i in range(n))
-    print(column)
-
+if len(head) // 2 != 0:
+    print(len(head)//2)
+elif True:
+    pass
