e34df795f41066de4866360d1948f87c63807804
Oleksii <celect1989@gmail.com>
2025-04-21 15:27:12 +0300

    LeetCode - next tasks v=1.0.0.2 21/04/2025

--- Python/lists/test.py
+++ Python/lists/test.py
@@ -1,27 +1,4 @@
-temperatures = [73,74,75,71,69,72,76,73]
+temperatures = [34,80,80,80,34,80,80,80,34,34]
 
-res = []
-
-if len(temperatures) == 0:
-    res.append(0)
-elif len(temperatures) > 0:
-    c = 0
-    for i in range(len(temperatures) - 1):
-        for t in range(i+1, len(temperatures)):
-            t1 = temperatures[i]
-            t2 = temperatures[t]
-            if temperatures[i] <= temperatures[t]:
-                c += 1
-                res.append(c)
-                break
-            elif temperatures[i] > temperatures[t]:
-                c += 1
-                if len(temperatures) - i == 2:
-                    res.append(0)
-
-        c = 0
-    res.append(0)
-
-
-
-print(res)
+for i in range(len(temperatures) - 2, -1, -1):
+    print(i, "-", temperatures[i])
